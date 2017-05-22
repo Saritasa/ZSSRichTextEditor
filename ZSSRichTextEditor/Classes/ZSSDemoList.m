f162706eedb8ed0eb9a53e636fd08809b2e096b3
Aleksey Kuznetsov <zummenix@gmail.com>
2017-05-22 10:02:32 +0500

    Remove examples

--- ZSSRichTextEditor/Classes/ZSSDemoList.m
+++ ZSSRichTextEditor/Classes/ZSSDemoList.m
@@ -7,27 +7,12 @@
 //
 
 #import "ZSSDemoList.h"
-#import "ZSSDemoViewController.h"
-#import "ZSSColorViewController.h"
-#import "ZSSLargeViewController.h"
-#import "ZSSPlaceholderViewController.h"
-#import "ZSSSelectiveViewController.h"
-#import "ZSSCustomButtonsViewController.h"
 
 @interface ZSSDemoList ()
 @property (nonatomic) BOOL isIPad;
 @end
 
 @implementation ZSSDemoList
-
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
@@ -37,12 +22,6 @@
     
     self.isIPad = ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad );
     
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
@@ -101,25 +80,11 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
     if (indexPath.row == 0) {
-        ZSSDemoViewController *demo1 = [[ZSSDemoViewController alloc] init];
-        [self.navigationController pushViewController:demo1 animated:YES];
     } else if (indexPath.row == 1) {
-        ZSSColorViewController *demo2 = [[ZSSColorViewController alloc] init];
-        [self.navigationController pushViewController:demo2 animated:YES];
     } else if (indexPath.row == 2) {
-        ZSSSelectiveViewController *demo3 = [[ZSSSelectiveViewController alloc] init];
-        [self.navigationController pushViewController:demo3 animated:YES];
     } else if (indexPath.row == 3) {
-        ZSSCustomButtonsViewController *demo4 = [[ZSSCustomButtonsViewController alloc] init];
-        [self.navigationController pushViewController:demo4 animated:YES];
     } else if (indexPath.row == 4) {
-        ZSSLargeViewController *demo5 = [[ZSSLargeViewController alloc] init];
-        [self.navigationController pushViewController:demo5 animated:YES];
     } else if (indexPath.row == 5) {
-        ZSSDemoViewController *demo1 = [[ZSSDemoViewController alloc] init];
-        UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:demo1];
-        nav.modalPresentationStyle = UIModalPresentationFormSheet;
-        [self presentViewController:nav animated:YES completion:nil];
     }
     
 }
